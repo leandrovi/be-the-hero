e2594987f347ba9bca4a84774c46dfe8b775d989
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-24 09:11:25 -0300

    feat: configuring database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,12 @@
 const express = require("express");
 
-const app = express();
+const routes = express.Router();
 
-app.use(express.json());
-
-app.get("/users", (request, response) => {
+routes.get("/users", (request, response) => {
   return response.json({
     event: "Semana Omnistack 11.0",
     student: "Leandro Vieira"
   });
 });
 
-app.listen(3333);
+module.exports = routes;
