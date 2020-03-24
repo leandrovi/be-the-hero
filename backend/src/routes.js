5ed24113bfda7a2436163e731a5aa68690fac12b
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-24 09:56:29 -0300

    feat: create ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const crypto = require("crypto");
+
+const connection = require("./database/connection");
 
 const routes = express.Router();
 
-routes.get("/users", (request, response) => {
-  return response.json({
-    event: "Semana Omnistack 11.0",
-    student: "Leandro Vieira"
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
   });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
