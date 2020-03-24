9d7bac72895ee7dd257ca8dd6790c3809560a45f
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-24 10:06:16 -0300

    feat: list ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,10 @@
 const express = require("express");
-const crypto = require("crypto");
 
-const connection = require("./database/connection");
+const OngController = require("./controllers/OngController");
 
 const routes = express.Router();
 
-routes.post("/ongs", async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  await connection("ongs").insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  });
-
-  return response.json({ id });
-});
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
 
 module.exports = routes;
