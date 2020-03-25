5d6031658f88a9ab3767a2875533dca81cbfa4c6
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-25 16:06:02 -0300

    feat: add currency format

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -52,7 +52,12 @@ export default function Profile() {
             <p>{incident.description}</p>
 
             <strong>VALOR:</strong>
-            <p>{incident.value}</p>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(incident.value)}
+            </p>
 
             <button type="button">
               <FiTrash2 size={20} color="#a8a8b3" />
