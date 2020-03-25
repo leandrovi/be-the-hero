b814852578373b7d60fea0b11c0252b71cfefd6d
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-25 16:13:21 -0300

    feat: handle incident delete

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -26,6 +26,20 @@ export default function Profile() {
       });
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert("Erro ao deletar caso, tente novamente.");
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -59,7 +73,10 @@ export default function Profile() {
               }).format(incident.value)}
             </p>
 
-            <button type="button">
+            <button
+              onClick={() => handleDeleteIncident(incident.id)}
+              type="button"
+            >
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
