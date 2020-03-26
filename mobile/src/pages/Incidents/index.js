42b9b1a81955139320f7854c62df0be0b5659c6b
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-26 16:21:52 -0300

    feat: add infinite scrolling with pagination

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -19,6 +19,8 @@ import styles from "./styles";
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
@@ -27,10 +29,24 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const response = await api.get("incidents");
+    if (loading) {
+      return;
+    }
 
-    setIncidents(response.data);
+    if (total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get("incidents", {
+      params: { page }
+    });
+
+    setIncidents([...incidents, ...response.data]);
     setTotal(response.headers["x-total-count"]);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -58,6 +74,8 @@ export default function Incidents() {
         data={incidents}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
