cb96c0436b08cb0dbc25bfaef310a5f76b769115
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-26 13:04:41 -0300

    feat: define initial expo structure

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  title: {
+    color: "#FFF",
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
